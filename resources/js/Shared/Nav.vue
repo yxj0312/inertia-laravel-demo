be3814d6d0ae68069191bd6bf12e5af6f24affa8
yxj0312 <yanxiajun@hotmail.com>
2021-10-30 23:34:40 +0200

    refactor with NavLink

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -2,42 +2,23 @@
     <nav class="mt-6">
         <ul class="list-disc">
             <li>
-                <Link
-                    href="/"
-                    class="text-blue-500 hover:underline"
-                    :class="{ 'font-bold underline': $page.component === 'Home' }"
-                >Home</Link>
+                <NavLink href="/" :active="$page.component === 'Home'">Home</NavLink>
             </li>
+
             <li>
-                <Link
-                    href="/users?foo=bar&baz=buzz"
-                    class="text-blue-500 hover:underline"
-                    :class="{ 'font-bold underline': $page.component === 'Users' }"
-                >Users</Link>
+                <NavLink href="/users" :active="$page.component === 'Users'">Users</NavLink>
             </li>
+
             <li>
-                <Link
-                    href="/settings"
-                    class="text-blue-500 hover:underline"
-                    :class="{ 'font-bold underline': $page.component === 'Settings' }"
-                >Settings</Link>
-            </li>
-            <li>
-                <Link
-                    href="/logout"
-                    method="post"
-                    :data="{ foo: 'bar' }"
-                    as="button"
-                    class="text-blue-500 hover:underline"
-                >Log Out</Link>
+                <NavLink href="/settings" :active="$page.component === 'Settings'">Settings</NavLink>
             </li>
         </ul>
     </nav>
 </template>
 
 <script>
-import { Link } from "@inertiajs/inertia-vue3";
+import NavLink from "./NavLink.vue";
 export default {
-    components: { Link }
+    components: { NavLink }
 };
 </script>
