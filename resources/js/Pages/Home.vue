1e2f90d2a6ab7b5131031df74c0d278499929fea
yxj0312 <yanxiajun@hotmail.com>
2021-11-04 23:12:40 +0100

    add Persistent Layouts

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,13 +1,10 @@
 <template>
-
-  <Layout>
-    <h1 class="text-3xl">Home</h1>
-  </Layout>
+  <h1 class="text-3xl">Home</h1>
 </template>
 
 <script>
-import Layout from "../Shared/Layout.vue"
+import Layout from "../Shared/Layout";
 export default {
-  components: { Layout }
+  layout: Layout
 };
 </script>
