76a140ab99cead77baa9a2ee5641c0c5085372c5
yxj0312 <yanxiajun@hotmail.com>
2021-11-13 22:39:14 +0100

    fetch users in the router

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -20,7 +21,8 @@ Route::get('/', function () {
 
 Route::get('/users', function () {
     return Inertia::render('Users', [
-        'time' => now()->toTimeString()
+        'time' => now()->toTimeString(),
+        'users' => User::all()
     ]);
 });
 
