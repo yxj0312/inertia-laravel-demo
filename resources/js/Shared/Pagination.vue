df621054bd208243bcd602dd8b72407a056cd96f
yxj0312 <yanxiajun@hotmail.com>
2021-11-15 22:53:16 +0100

    extrat to pagination component

---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        props: {
+            links: Array
+        }
+    }
+</script>
+
+<template>
+  <div>
+    <Component
+      :is="link.url ? 'Link' : 'Span'"
+      v-for="link in users.links"
+      :href="link.url"
+      v-html="link.label"
+      class="px-1"
+      :class="link.url ? '' : 'text-gray-500'"
+    />
+  </div>
+</template>
