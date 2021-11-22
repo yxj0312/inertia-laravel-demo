8b0c48163b81aceaa03752c301fbde3d9851bd6c
yxj0312 <yanxiajun@hotmail.com>
2021-11-22 22:40:47 +0100

    get create rendering

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class LoginController extends Controller
 {
     public function create()
     {
-        return 'please login';
+        return Inertia::render('Auth/Login');
     }
 }
