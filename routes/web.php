ad578bb2e6ccc7b003d1195be481e9ba60b057f4
yxj0312 <yanxiajun@hotmail.com>
2021-11-25 22:02:23 +0100

    add logout route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Inertia\Inertia;
 
 Route::get('login', [LoginController::class, 'create'])->name('login');
 Route::post('login', [LoginController::class, 'store']);
+Route::post('logout', [LoginController::class, 'destroy'])->middleware('auth');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
