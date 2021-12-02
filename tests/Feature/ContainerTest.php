5da55566275a2861b6f7def025d2c9bdfd5aee82
yxj0312 <yanxiajun@hotmail.com>
2021-12-02 22:59:59 +0100

    first test

---
+++ tests/Feature/ContainerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContainerTest extends TestCase
+{
+   /** @test */
+   function its_a_toy_chest()
+   {
+       $container = [
+           'foo' => 'bar'
+       ];
+
+       var_dump($container['foo']);
+   }
+}
