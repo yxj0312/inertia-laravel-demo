9fdb9d180979ae651c6d155577f3c6a14b63118d
yxj0312 <yanxiajun@hotmail.com>
2021-12-06 22:56:36 +0100

    add test2

--- app/Newsletter.php
+++ app/Newsletter.php
@@ -4,5 +4,15 @@ namespace App;
 
 class Newsletter
 {
-    
+    protected array $bindings = [];
+
+    public function bind($key, $value)
+    {
+        $this->bindings[$key] = $value;
+    }
+
+    public function get($key)
+    {
+        return $this->bindings[$key];
+    }
 }
