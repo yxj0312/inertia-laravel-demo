ee7eb85d85a5f7655a1ca59387e17c0cb0318cf4
yxj0312 <yanxiajun@hotmail.com>
2021-12-09 22:01:09 +0100

    remove newsletter

--- app/Newsletter.php
+++ app/Newsletter.php
@@ -6,19 +6,4 @@ use Closure;
 
 class Newsletter
 {
-    protected array $bindings = [];
-
-    public function bind($key, $value)
-    {
-        $this->bindings[$key] = $value;
-    }
-
-    public function get($key)
-    {
-        $concrete = $this->bindings[$key];
-        if ($concrete instanceof Closure) {
-            return $concrete();
-        }
-        return $concrete;
-    }
 }
