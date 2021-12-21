f46910490bc262e76414fb9df121bca5981c8768
yxj0312 <yanxiajun@hotmail.com>
2021-12-21 23:04:42 +0100

    refactore test

---
+++ tests/Unit/ContainerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Container;
+use App\Http;
+use App\Mailchimp;
+use App\Newsletter;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use function PHPUnit\Framework\assertInstanceOf;
+
+class ContainerTest extends TestCase
+{
+//    /** @test */
+//    function its_a_toy_chest()
+//    {
+//        $container = [
+//            'foo' => 'bar'
+//        ];
+
+//        var_dump($container['foo']);
+//    }
+
+//    /** @test */
+//    function its_a_toy_chest()
+//    {
+//        $container = [
+//            'foo' => function() {
+//                return 'bar';
+//            }
+//        ];
+
+//        var_dump($container['foo']());
+//    }
+
+    /** @test */
+    function LEVEL_ONE_its_like_a_toy_chest()
+    {
+        $container = new Container();
+
+        $container->bind('foo', 'bar');
+
+        $this->assertEquals('bar', $container->get('foo'));
+    }
+
+   /** @test */
+    function LEVEL_TWO_it_can_resolve_functions_and_singletons()
+    {
+        $container = new Container();
+
+        $container->bind('newsletter', function () {
+            return new Newsletter(
+                new Mailchimp(new Http())
+            );
+        });
+
+        $this->assertInstanceOf(Newsletter::class, $container->get('newsletter'));
+
+        $container->singleton('newsletter', function () {
+            return new Newsletter(
+                new Mailchimp(new Http())
+            );
+        });
+
+        $newsletter1 = $container->get('newsletter');
+        $newsletter2 = $container->get('newsletter');
+
+        $this->assertSame($newsletter1, $newsletter2);
+    }
+
+    /** @test */
+    function LEVEL_THREE_it_can_do_magic()
+    {
+        $container = new Container();
+
+        $this->assertInstanceOf(Newsletter::class, $container->get(Newsletter::class));
+    }
+}
